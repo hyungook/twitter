8ca6c9b12f618e83ad04eeed4521a10439b3b6f5
오형욱 <nenonenoberneno@gmail.com>
2021-02-03 01:05:59 +0900

    Log Out

--- src/components/router.js
+++ src/components/router.js
@@ -1,22 +1,34 @@
 import React, { useState } from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import Auth from '../routes/auth';
 import Home from '../routes/home';
+import Profile from '../routes/profile';
+import Navigation from './navigation';
 
 
 const AppRouter =  ({isLoggedIn}) => {
     
     return(
         <Router>
+            {isLoggedIn && <Navigation />}
             <Switch>
                 {isLoggedIn ? (
                 <>
                     <Route exact path="/">
                         <Home />
                     </Route>
-                </>) : (<Route exact path="/">
-                        <Auth />
-                    </Route>)}
+                    <Route exact path="/profile">
+                        <Profile />
+                    </Route>
+                    <Redirect from="*" to="/" />
+                </>) : (
+                    <>
+                        <Route exact path="/">
+                            <Auth />
+                        </Route>
+                        <Redirect from="*" to="/" />
+                    </>
+                    )}
             </Switch>
         </Router>
     )
