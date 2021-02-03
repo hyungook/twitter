17973cb0d679b577881343be0ee8b8054efafcb1
오형욱 <nenonenoberneno@gmail.com>
2021-02-04 00:11:08 +0900

    Delete and Update part two

--- src/components/tweet.js
+++ src/components/tweet.js
@@ -1,12 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { dbService } from '../firebase';
 
-const Tweet = ({tweetObj, isOwner}) => (
-        <div key={tweetObj.id}>
-            <h4>{tweetObj.text}</h4>
-            {/* 작성자면 아래 button이 보인다 */}
-            {isOwner && <><button>Delete Tweet</button>
-            <button>Edit Tweet</button></>}
-        </div>
-    );
+const Tweet = ({tweetObj, isOwner}) => {
+    const [editing, setEditing] = useState(false);
+    const [newTweet, setNewTweet] = useState(tweetObj.text);
+
+    const onDeleteClick = async () => {
+        const ok = window.confirm("Are you sure you want to delete this tweet?");
+        console.log(ok)
+        if(ok) {
+            //delete tweet
+            await dbService.doc(`tweets/${tweetObj.id}`).delete();
+        }
+    };
+
+    const toggleEditing = () => setEditing((prev) => !prev);
+
+    const onSubmit =  async (event) => {
+        event.preventDefault();
+        console.log(tweetObj, newTweet);
+        await dbService.doc(`tweets/${tweetObj.id}`).update({
+            text:newTweet,
+        })
+        setEditing(false);
+    }
+
+    const onChange = (event) => {
+        const {target: {value},} = event;
+        setNewTweet(value);
+    }
+
+    return (
+        <div>
+      {editing ? (
+        <>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              placeholder="Edit your tweet"
+              value={newTweet}
+              required
+              onChange={onChange}
+            />
+            <input type="submit" value="Update Tweet" />
+          </form>
+          <button onClick={toggleEditing}>Cancel</button>
+        </>
+      ) : (
+        <>
+          <h4>{tweetObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete Nweet</button>
+              <button onClick={toggleEditing}>Edit Nweet</button>
+            </>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Tweet;
