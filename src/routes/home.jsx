e2e15723555a99d47b0929ec534611f698d6329e
오형욱 <nenonenoberneno@gmail.com>
2021-02-03 21:52:15 +0900

    Getting the Tweets

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,9 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { dbService } from '../firebase';
 
 const Home = () => {
 
     const [tweet, setTweet] = useState("");
+    const [tweets, setTweets] = useState([]);
+
+    const getTweets = async() => {
+        const dbTweets = await dbService.collection("tweets").get();
+        // console.log(dbTweets);
+        dbTweets.forEach((document) => {
+            const tweetObject = {
+                ...document.data(),
+                id: document.id,
+            }
+            setTweets(prev => [tweetObject, ...prev])
+        });
+    }
+
+    useEffect(() => {
+        getTweets();
+    }, []);
+
+
 
     const onSubmit = async (event) => {
         event.preventDefault();
@@ -13,17 +32,27 @@ const Home = () => {
         });
         setTweet("");
     }
-    
+
     const onChange = (event) => {
         const {target:{value}} = event;
         setTweet(value);
     }
+
+    console.log(tweets);
+
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <input value={tweet} onChange={onChange} type="text" placeholder="what's on your mind?" maxLength={120} />
                 <input type="submit" value="Tweet" />
             </form>
+            <div>
+                {tweets.map(tweet => (
+                    <div key={tweet.id}>
+                        <h4>{tweet.tweet}</h4>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
